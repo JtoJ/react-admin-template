8be47082e00a062959b2b53fbc01ab85a98e14d9
JtoJ <13924032551@139.com>
2019-03-18 22:59:57 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Layout from './views/layout/layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout></Layout>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/layout/layout.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './layout.css';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+export default class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    return (
+      <Layout className="root-layout">
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className="root-header">
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content className="root-content">
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+// ReactDOM.render(<SiderDemo />, mountNode);
